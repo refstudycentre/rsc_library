385478f47e64a6665e3eef83df30e30b72bce701
dolf <rudolfbyker@gmail.com>
2016-05-26 15:08:35 +0200

    Busy refactoring and expanding the node preprocess function

--- templates/node--rscl-block.tpl.php
+++ templates/node--rscl-block.tpl.php
@@ -1,7 +1,8 @@
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <?php
-    print render($content['field_image']); // RC
-    print render($content['field_featured_image']); // CL
+//    print render($content['field_image']); // TODO: remove (RC legacy)
+//    print render($content['field_featured_image']); // TODO: remove (CL legacy)
+    print render($content[RSC_LIBRARY_PREFIX.'_featured_image']);
   ?>
   <header>
     <?php print render($title_prefix); ?>
