dff13cce0770b1af1d32ebac764a5bbd722dd125
dolf <rudolfbyker@gmail.com>
2016-12-02 22:45:20 +0200

    Removed legacy code

--- templates/node--rscl-block.tpl.php
+++ templates/node--rscl-block.tpl.php
@@ -1,9 +1,5 @@
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <?php
-//    print render($content['field_image']); // TODO: remove (RC legacy)
-//    print render($content['field_featured_image']); // TODO: remove (CL legacy)
-    print render($content[RSC_LIBRARY_PREFIX.'_featured_image']);
-  ?>
+  <?php print render($content[RSC_LIBRARY_PREFIX.'_featured_image']); ?>
   <header>
     <?php print render($title_prefix); ?>
     <h3<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h3>
